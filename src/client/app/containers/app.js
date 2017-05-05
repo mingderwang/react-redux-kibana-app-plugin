a52e0acbc19ce89bcc941084e6b5ae7f23dee85e
Ming-der Wang <ming@log4analytics.com>
2017-05-05 12:16:20 +0800

    ignore DS_Store

--- src/client/app/containers/app.js
+++ src/client/app/containers/app.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Counter from '../components/counter_mapping'
 import Adder from '../components/add_mapping'
-import { createStore, compose, applyMiddleware } from 'redux';
 import DevTools from '../utils/DevTools'
-
 
 const App = () => {
   return (

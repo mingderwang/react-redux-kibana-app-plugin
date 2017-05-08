166f80052b22e26daf0563c10709b07cb80d29f0
Ming-der Wang <ming@log4analytics.com>
2017-05-08 12:11:59 +0800

    ok

--- src/client/app/components/add_mapping.js
+++ src/client/app/components/add_mapping.js
@@ -7,9 +7,9 @@ import { connect } from 'react-redux'
  * 對應 Redux 的 state 到 component 的 props
  */
 
-const mapStateToProps = (...args) => { // can replace ...args with state
+const mapStateToProps = (state) => {
   return {
-    counter_state: args[0].add_state
+    add_state: state.add_state
   };
 };
 
